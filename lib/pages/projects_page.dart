c02ae6b16b490f6372baae7ce58f4ff4beb60114
bardia <bardiakzm@gmail.com>
2024-09-22 14:06:50 -0700

    lazyCommit

--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -3,8 +3,8 @@ import 'dart:ui_web';
 import 'package:peyazma_web/resources/lists/geotechnical_projects_list.dart';
 import 'package:peyazma_web/resources/lists/qaulity_control_and_local_unit_list.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:peyazma_web/widgets/option_bar.dart';
+import 'package:peyazma_web/resources/bottom_page_information.dart';
 
 class ProjectsPage extends StatefulWidget {
   const ProjectsPage({super.key});
@@ -38,9 +38,9 @@ class _ProjectsPageState extends State<ProjectsPage> {
             children: [
               _buildProjectSection(),
               const SizedBox(height: 32),
-              _buildMapSection(context),
+              buildMapSection(context),
               const SizedBox(height: 32),
-              _buildContactDetailsSection(context),
+              buildContactDetailsSection(context),
             ],
           ),
         ),
@@ -187,125 +187,5 @@ class _ProjectsPageState extends State<ProjectsPage> {
         ]);
       }).toList(),
     );
-  }
-
-  // Map section widget
-  Widget _buildMapSection(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 250,
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-              ),
-              child: HtmlElementView(viewType: 'iframeElement'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Text(
-                  "موقعیت ما روی نقشه",
-                  style: Theme.of(context).textTheme.titleLarge,
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.open_in_new),
-                  label: const Text(
-                    'مشاهده نقشه کامل',
-                    style: TextStyle(color: Colors.white70),
-                  ),
-                  onPressed: _launchMap,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue.shade700,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 12),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Contact details section
-  Widget _buildContactDetailsSection(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              'اطلاعات تماس',
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall
-                  ?.copyWith(color: Colors.blue.shade800),
-            ),
-            const SizedBox(height: 24),
-            _buildContactItem(Icons.email, 'ایمیل', 'peyazma.k.s@gmail.com'),
-            _buildContactItem(Icons.phone, 'تلفن', '021-44593139'),
-            _buildContactItem(Icons.location_city, 'کد پستی', '1388853961'),
-            _buildContactItem(Icons.fax, 'فکس', '021-44593139'),
-            _buildContactItem(Icons.location_on, 'آدرس',
-                'تهرانسر، بلوار یاس، خیابان نفت جنوبی، کوچه 46، پلاک 15'),
-            _buildContactItem(Icons.access_time, 'ساعات کاری', '8:00-16:30'),
-          ],
-        ),
-      ),
-    );
-  }
-
-  // Contact item
-  Widget _buildContactItem(IconData icon, String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Expanded(
-            child: Text(
-              value,
-              style: const TextStyle(fontSize: 16),
-              textAlign: TextAlign.right,
-            ),
-          ),
-          const SizedBox(width: 8),
-          Text(
-            '$label:',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue.shade700,
-            ),
-          ),
-          const SizedBox(width: 8),
-          Icon(icon, color: Colors.blue.shade700),
-        ],
-      ),
-    );
-  }
-
-  // Launch the full map link
-  void _launchMap() async {
-    final Uri url =
-        Uri.parse('https://www.google.com/maps?ll=35.687987,51.256856&z=17');
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      throw 'Could not launch $url';
-    }
   }
 }
